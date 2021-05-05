fe88ae21a095c396f1ae2d1c5b0b524b0220ce65
DaniilPanasenko <daniil.panasenko.work@gmail.com>
2021-05-05 22:42:56 +0300

    Initial Commit

---
+++ DocUp-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DocUp-ios
+//
+//  Created by админ on 5/5/21.
+//  Copyright © 2021 админ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
